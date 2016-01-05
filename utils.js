3c1d6856fc2c7eead409132627b2ba2ea62dde73
WebDevStu <git@stewart-anderson.co.uk>
2016-01-05 21:16:04 +0000

    Setting up utility methods for later

--- utils.js
+++ utils.js
@@ -3,6 +3,52 @@
 
 var _ = {
 
+    events: {},
 
 
+    listenTo: function (bindings, scope) {
+
+        _.forEach(bindings, function (method, binding) {
+
+            scope[binding]._scope = scope;
+
+            _.events[prop] = _.events[prop] || [];
+            _.events[prop].push(scope[binding]);
+        });
+    },
+
+
+    trigger: function (trigger) {
+
+        if (_.events[trigger]) {
+            _.forEach(_.events[trigger], function (fn) {
+                if (fn._scope) {
+                    fn.call(fn._scope);
+                } else {
+                    fn();
+                }
+            });
+        }
+    },
+
+
+    forEach: function (item, callback, scope) {
+
+        var prop;
+
+        if (typeof item !== 'object') {
+            throw new Error('not an iteratable source');
+        }
+
+        if (item instanceof Array) {
+            item.forEach(callback);
+        } else {
+
+            for (prop in item) {
+                if (item.hasOwnProperty(prop)) {
+                    callback(item[prop], prop);
+                }
+            }
+        }
+    }
 };
