ba3d7ee89e68061a7cf9a93cb8c0ab518106f002
WebDevStu <git@stewart-anderson.co.uk>
2016-01-06 20:49:43 +0000

    Setting up snake class

--- canvas.js
+++ canvas.js
@@ -5,17 +5,22 @@
  */
 var Canvas = function () {
 
+    this.frame = 0;
+    this.speed = 60;
+
     // canvas
     this.canvas = document.createElement('canvas');
     // attrs
-    this.canvas.height = 400;
-    this.canvas.width = 300;
+    this.canvas.width = 400;
+    this.canvas.height = 300;
     this.canvas.className = 'board';
 
-    // context
+    // context & append
     this.ctx = this.canvas.getContext('2d');
-
     document.body.appendChild(this.canvas);
+
+    // new snake for the board
+    this.snake = new Snake(this.ctx);
 
     // listen events
     _.listenTo({
@@ -26,5 +31,10 @@ var Canvas = function () {
 
 Canvas.prototype.draw = function () {
 
-    //console.log('rofl', this);
+    this.frame += 1;
+
+    if (this.frame >= this.speed) {
+        this.frame = 0;
+
+    }
 };
--- snake.js
+++ snake.js
@@ -3,12 +3,76 @@
  *
  * @constructor
  */
-var Snake = function () {
+var Snake = function (ctx) {
+
+    this.ctx = ctx;
+    this.tail = [{
+        x: 198,
+        y: 148
+    }];
+
+    this.createSegment();
+};
+
+
+/**
+ * move
+ * called from the Canvas class passing a direction of travel, appends a segment
+ * to the start of the tail array and removes from the end
+ * @param direction
+ */
+Snake.prototype.move = function (direction) {
+
+    // add to start (dependant of direction)
+    this.tail.unshift({
+        x: 100,
+        y: 100
+    });
+
+    // remove last if no food eaten
+    this.tail.pop();
 
 
 };
 
 
-Snake.prototype.foo = function () {
+/**
+ * drawSnake
+ */
+Snake.prototype.drawSnake = function () {
+
+    _.forEach(this.tail, function (config) {
+        this.createSegment(config.x, config.y);
+    }, this);
+};
+
+
+/**
+ * createSegment
+ * creates a section of the snake
+ *
+ * @param x
+ * @param y
+ */
+Snake.prototype.createSegment = function (x, y) {
+
+    x = x || 198;
+    y = y || 148;
+
+    this.ctx.beginPath();
+    this.ctx.rect(x, y, 4, 4);
+    this.ctx.fillStyle = 'black';
+    this.ctx.fill();
+};
+
+
+
+
+
+
+Snake.prototype.add = function () {
 
 };
+
+
+
